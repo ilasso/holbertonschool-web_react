305160d3c1e66e409a9d66c21aeee5669ecca1b3
ilasso <1204@holbertonschool.com>
2021-02-09 12:41:33 -0500

    include thead

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -25,9 +25,26 @@ const student2:Student = new Student("Andres","Lasso", 16,"Cali");
 
 const students = [student1, student2];
 
+/* vanilla*/
 const table = document.createElement('table');
+
+const thead: HTMLTableSectionElement = document.createElement('thead');
+const trhead: HTMLTableRowElement = document.createElement('tr');
+const trhfirstName: HTMLTableCellElement = document.createElement('td');
+const trhlocation: HTMLTableCellElement = document.createElement('td');
+
+table.append(thead);
+trhfirstName.innerHTML = 'firstName';
+trhlocation.innerHTML = 'location';
+thead.append(trhead);
+trhead.append(trhfirstName);
+trhead.append(trhlocation);
+
+const tbody: HTMLTableSectionElement = document.createElement('tbody');
+table.append(tbody)
+
 students.forEach((student) => {
-    let row = table.insertRow()
+    let row = tbody.insertRow()
     for (let [k, v] of Object.entries(student)) {
         if (k === 'firstName' || k === 'location') {
             let cell = row.insertCell()
