f53c108815fdd6d005cb39eefb48c0305b5d8861
ilasso <1204@holbertonschool.com>
2021-02-08 23:19:27 -0500

    String literal types

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -79,3 +79,15 @@ function executeWork(employee:Director|Teacher): string{
 console.log(executeWork(createEmployee(1000)));
 */
 
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass:Subjects) {
+    if(todayClass === 'Math')
+        return 'Teaching Math';
+    else 
+        return 'Teaching History';
+}
+
+// console.log(teachClass('hola'));
+// console.log(teachClass('Math'));
+// console.log(teachClass('History'));
