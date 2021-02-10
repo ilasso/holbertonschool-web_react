e2af942f462e49d86d77e8240f4ac88c4201a25a
ilasso <1204@holbertonschool.com>
2021-02-10 10:41:27 -0500

    types, import

--- 0x00-TypeScript/task_3/js/crud.d.ts
+++ 0x00-TypeScript/task_3/js/crud.d.ts
@@ -1,7 +1,13 @@
 import { RowID, RowElement } from './interface';
 
-export type updateRow = (rowId: RowID, row: RowElement) => number;
+/*export type updateRow = (rowId: RowID, row: RowElement) => number;
 
 export type deleteRow = (rowId: RowID) => void;
 
-export type insertRow = (row: RowElement) => number;
+export type insertRow = (row: RowElement) => number;*/
+
+export function updateRow(rowId: RowID, row: RowElement): RowID;
+
+export function deleteRow(rowId: RowID): void;
+
+export function insertRow(row: RowElement): RowID;
