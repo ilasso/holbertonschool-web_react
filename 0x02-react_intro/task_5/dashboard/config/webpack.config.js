05c552ab1c6fff19975093f8b146b1e83be4df3c
ilasso <1204@holbertonschool.com>
2021-02-17 16:43:40 -0500

    poc

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -5,16 +5,16 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        index: path.resolve(__dirname,'../src/index.js'),
+        index: path.resolve(__dirname,'./src/index.js'),
     },
     mode: 'development',
     output: {
-        path: path.resolve(__dirname, '../dist'),
+        path: path.resolve(__dirname, './dist'),
         filename: 'bundle.js'
     },
     devtool: 'inline-source-map',
     devServer: {
-     contentBase: path.join(__dirname, '../public'),
+     contentBase: path.join(__dirname, './dist'),
      hot:true, 
      open:true,
      port:8564,  
@@ -53,7 +53,7 @@ module.exports = {
         new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPlugin({
             title: 'webpack-dev-server',
-            template: path.resolve(__dirname, '../public/index.html')
+            template: path.resolve(__dirname, './public/index.html')
         }),
      ]
 }
