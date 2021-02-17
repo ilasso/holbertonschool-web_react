dcbb3be1ac005ee8718f8eb9aea02c45d1dc8b32
ilasso <1204@holbertonschool.com>
2021-02-17 17:16:14 -0500

    poc

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,59 +1,43 @@
-const path = require('path')
-const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const path = require("path");
 
 module.exports = {
-    entry: {
-        index: path.resolve(__dirname,'./src/index.js'),
-    },
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js'
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-     contentBase: path.join(__dirname, './dist'),
-     hot:true, 
-     open:true,
-     port:8564,  
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
-                
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("./dist"),
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.resolve("./dist"),
+    compress: true,
+    port: 8564,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
             },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                  'file-loader',
-                  {
-                    loader: 'image-webpack-loader',
-                    options: {
-                      bypassOnDebug: true, // webpack@1.x
-                      disable: true, // webpack@2.x and newer
-                    },
-                  },
-                ],
-              }
-        ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'webpack-dev-server',
-            template: path.resolve(__dirname, './public/index.html')
-        }),
-     ]
-}
+          },
+        ],
+      },
+    ],
+  },
+};
