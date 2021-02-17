ddbf897b22fa14f09ea31495eea595d8146cee8c
ilasso <1204@holbertonschool.com>
2021-02-17 00:41:23 -0500

    Create basic tests with four tests

---
+++ 0x02-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,26 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+import UserConnect from './components/auth'
+
+import { getFooterCopy, getFullYear }  from './utils';
+function App() {
+  let footertext = getFooterCopy(true);
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <UserConnect />
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {footertext}</p>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
