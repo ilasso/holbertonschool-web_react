02cced3d5771563a0fcd4fb7ef8e30bdfcdd8d03
ilasso <1204@holbertonschool.com>
2021-02-25 23:08:02 -0500

    Reusable comments & specialization

---
+++ 0x04-React_component/task_3/dashboard/src/BodySection/BodySection.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class BodySection extends Component {
+    render() {
+        const {title} = this.props;
+        return (
+            <div className="bodySection">
+                <h2>{title}</h2>
+                <p>{title}</p>
+            </div>
+        );
+    }
+}
+
+BodySection.propTypes = {
+    title: PropTypes.string
+};
+
+
+export default BodySection;
