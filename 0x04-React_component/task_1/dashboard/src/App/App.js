ceb90f77aa6519db6a1e3056711f4f9f820714d7
ilasso <1204@holbertonschool.com>
2021-02-28 19:32:10 -0500

    poc9

--- 0x04-React_component/task_1/dashboard/src/App/App.js
+++ 0x04-React_component/task_1/dashboard/src/App/App.js
@@ -3,7 +3,7 @@ import Login from '../Login/Login'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import Notifications from '../Notifications/Notifications';
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import CourseList from '../CourseList/CourseList';
 
